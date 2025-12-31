295502dbbeca20a25e063825fde19e16542a4077
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-31 16:27:41 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,6 @@ variable "app_name" {
   default = "hostname-version-app"
 }
 
-variable "image" {
-  default = "035466343132.dkr.ecr.ap-south-1.amazonaws.com/hostname-version-app:1.1"
+variable "image_tag" {
+  default = "latest"
 }

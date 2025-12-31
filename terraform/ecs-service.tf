33ef358718bcf8d2da83e623c2e4baa3048097e5
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-31 14:58:30 +0530

    Update ecs-service.tf

--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -1,8 +1,8 @@
-# ---------- Service 01 ----------
+# ECS Service 01  
 resource "aws_ecs_service" "service_01" {
   name            = "hostname-service-01"
   cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.task.arn
+  task_definition = aws_ecs_task_definition.task_01.arn
   desired_count   = 1
   launch_type     = "EC2"
 
@@ -13,32 +13,21 @@ resource "aws_ecs_service" "service_01" {
   }
 
   network_configuration {
-    subnets         = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+    subnets         = [
+      aws_subnet.public_1.id,
+      aws_subnet.public_2.id
+    ]
     security_groups = [aws_security_group.ecs_sg.id]
   }
 
-  deployment_controller {
-    type = "ECS"
-  }
-
   enable_execute_command = true
-
-  overrides = jsonencode({
-    containerOverrides = [{
-      name = "app"
-      environment = [
-        { name = "NODE_NUMBER", value = "01" },
-        { name = "APP_VERSION", value = "1.0" }
-      ]
-    }]
-  })
 }
 
-# ---------- Service 02 ----------
+# ECS Service 02  
 resource "aws_ecs_service" "service_02" {
   name            = "hostname-service-02"
   cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.task.arn
+  task_definition = aws_ecs_task_definition.task_02.arn
   desired_count   = 1
   launch_type     = "EC2"
 
@@ -49,28 +38,21 @@ resource "aws_ecs_service" "service_02" {
   }
 
   network_configuration {
-    subnets         = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+    subnets         = [
+      aws_subnet.public_1.id,
+      aws_subnet.public_2.id
+    ]
     security_groups = [aws_security_group.ecs_sg.id]
   }
 
   enable_execute_command = true
-
-  overrides = jsonencode({
-    containerOverrides = [{
-      name = "app"
-      environment = [
-        { name = "NODE_NUMBER", value = "02" },
-        { name = "APP_VERSION", value = "2.0" }
-      ]
-    }]
-  })
 }
 
-# ---------- Service 03 ----------
+# ECS Service 03  
 resource "aws_ecs_service" "service_03" {
   name            = "hostname-service-03"
   cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.task.arn
+  task_definition = aws_ecs_task_definition.task_03.arn
   desired_count   = 1
   launch_type     = "EC2"
 
@@ -81,19 +63,12 @@ resource "aws_ecs_service" "service_03" {
   }
 
   network_configuration {
-    subnets         = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+    subnets         = [
+      aws_subnet.public_1.id,
+      aws_subnet.public_2.id
+    ]
     security_groups = [aws_security_group.ecs_sg.id]
   }
 
   enable_execute_command = true
-
-  overrides = jsonencode({
-    containerOverrides = [{
-      name = "app"
-      environment = [
-        { name = "NODE_NUMBER", value = "03" },
-        { name = "APP_VERSION", value = "3.0" }
-      ]
-    }]
-  })
 }

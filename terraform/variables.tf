d38557781d3c64ea5c9c2fc979886368b989929e
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-31 16:40:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,6 +14,12 @@ variable "image" {
   default     = "035466343132.dkr.ecr.ap-south-1.amazonaws.com/hostname-version-app:1.0"
 }
 
+variable "image_tag" {
+  description = "Docker image tag"
+  type        = string
+  default     = "1.0"
+}
+
 variable "region" {
   description = "AWS region"
   type        = string

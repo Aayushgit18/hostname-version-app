39bfcf46488f50560f0cc0e6ea735dc1f080557d
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-31 16:39:28 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,21 @@
+# ===============================
+# APPLICATION VARIABLES
+# ===============================
+
 variable "app_name" {
-  default = "hostname-version-app"
+  description = "Application name"
+  type        = string
+  default     = "hostname-version-app"
 }
 
-variable "image_tag" {
-  default = "latest"
+variable "image" {
+  description = "Docker image URI stored in ECR"
+  type        = string
+  default     = "035466343132.dkr.ecr.ap-south-1.amazonaws.com/hostname-version-app:1.0"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
 }

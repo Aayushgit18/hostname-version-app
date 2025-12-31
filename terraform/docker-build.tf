8ccc6b2a5404e55992f4180133ddb0b9b75c3cd0
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-31 16:52:00 +0530

    Update docker-build.tf

--- terraform/docker-build.tf
+++ terraform/docker-build.tf
@@ -1,11 +1,11 @@
 resource "null_resource" "docker_build_push" {
 
-  depends_on = [aws_ecr_repository.app]
-
   provisioner "local-exec" {
+    interpreter = ["PowerShell", "-Command"]
+
     command = <<EOT
-aws ecr get-login-password --region ap-south-1 |
-docker login --username AWS --password-stdin ${aws_ecr_repository.app.repository_url}
+aws ecr get-login-password --region ap-south-1 `
+| docker login --username AWS --password-stdin 035466343132.dkr.ecr.ap-south-1.amazonaws.com
 
 docker build -t ${var.app_name}:latest ..
 
@@ -14,5 +14,8 @@ docker tag ${var.app_name}:latest ${aws_ecr_repository.app.repository_url}:${var
 docker push ${aws_ecr_repository.app.repository_url}:${var.image_tag}
 EOT
   }
-}
 
+  depends_on = [
+    aws_ecr_repository.app
+  ]
+}
